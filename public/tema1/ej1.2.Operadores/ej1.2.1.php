a43e853507514ec40f17a6f9f33b89be2857b782
Valery Liste <valeryiste.loscardones@gmail.com>
2025-09-24 15:34:42 +0000

    ejercicio de operadores

---
+++ public/tema1/ej1.2.Operadores/ej1.2.1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $numero_string = "100";
+        $numero_entero = 25;
+
+        $resultado_suma = $numero_string + $numero_entero;
+        echo "Valor: $resultado_suma, Tipo: " . gettype($resultado_suma) . "\n";
+
+        $resultado_concat = $numero_string . $numero_entero;
+        echo "Valor: $resultado_concat, Tipo: " . gettype($resultado_concat);
+    ?>
+</body>
+</html>
